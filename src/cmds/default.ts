d5c5de468cd29fbea562d6d05fdaca90ed4a3334
Thien Ly <lyluongthien1002@gmail.com>
2022-02-18 15:09:57 +0700

    feat: read files

--- src/cmds/default.ts
+++ src/cmds/default.ts
@@ -1,23 +1,24 @@
 import type { Arguments, CommandBuilder } from 'yargs';
-
+import { readFileSync } from 'fs';
+import { FileEntity } from '../entities/fileEntity';
 type Options = {
     files?: string[] | string;
     outDir?: string
 };
 
-export const command: string[] = ['$0 [outDir] [files..]', 'compile [files..]'];
+export const command: string[] = ['$0 [outDir] [files..]', 'compile [outDir] [files..]'];
 export const desc: string = 'compile ts types to markdown files';
 
 
 export const builder: CommandBuilder<Options, Options> = (yargs) =>
     yargs
-    .positional('files..', {
-        describe: 'ts types files with only type and interfaces'
-    }) 
-    .positional('outDir', {
-        describe: 'output directory. Default: ./output',
-        default: './output'
-    }) 
+        .positional('files..', {
+            describe: 'ts types files with only type and interfaces'
+        })
+        .positional('outDir', {
+            describe: 'output directory. Default: ./output',
+            default: './output'
+        })
 
 
 export const handler = (argv: Arguments<Options>): void => {
@@ -26,13 +27,22 @@ export const handler = (argv: Arguments<Options>): void => {
         console.error("No file detected.")
         process.exit(1);
     }
-    if(!Array.isArray(files)){
+    if (!Array.isArray(files)) {
         files = [files]
     }
 
-    console.log( `args here:\n${JSON.stringify(argv, null, 2)}`);
-    console.log( `files here:\n${JSON.stringify(files, null, 2)}`);
-    //   process.stdout.write(outString);
- 
+    console.log(`args here:\n${JSON.stringify(argv, null, 2)}`);
+    console.log(`files here:\n${JSON.stringify(files, null, 2)}\n`);
+
+
+
+    const fileEntities:Array<FileEntity> = [];
+
+    files.forEach(path => {
+        fileEntities.push(new FileEntity(path));
+        console.log(fileEntities[fileEntities.length-1].preview());
+    });
+
+
     process.exit(0);
 };
---
+++ src/entities/fileEntity.ts
@@ -0,0 +1,13 @@
+import { readFileSync } from "fs";
+
+export class FileEntity { 
+    constructor(public path:string, public content: string | null = null){
+        if(!content){
+            const fileContent = readFileSync(path, 'utf-8');
+            this.content = fileContent
+        }
+    }
+    public preview(len:number=300):string{
+        return this.content!.slice(0,len) +"...[...]";
+    }
+}
